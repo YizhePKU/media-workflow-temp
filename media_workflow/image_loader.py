3b2323f8dbed4d0885783aa8d6fdc2e810c78393
sunyizhe <sunyizhe@tezign.com>
2024-11-18 11:04:16 +0800

    feat: support loading AVIF images

--- media_workflow/image_loader.py
+++ media_workflow/image_loader.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 from typing import BinaryIO
 
+import pillow_avif
 from cairosvg import svg2png
 from PIL import Image, UnidentifiedImageError
 from pillow_heif import register_heif_opener
@@ -15,6 +16,7 @@ def load_image(file: BinaryIO) -> Image:
     function also supports the following formats:
 
     HEIC: High Efficiency Image File
+    AVIF: AV1 Image File Format
     SVG: Scalable Vector Graphics
 
     [1]: https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html
