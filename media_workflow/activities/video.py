5b1c776337b0949a4220d35705bebc93fb97a1d2
sunyizhe <sunyizhe@tezign.com>
2024-12-09 14:46:18 +0800

    feat: update video-transcode

--- media_workflow/activities/video.py
+++ media_workflow/activities/video.py
@@ -1,6 +1,7 @@
 import math
+import tempfile
 from pathlib import Path
-from tempfile import TemporaryDirectory
+from uuid import uuid4
 
 import ffmpeg
 import numpy as np
@@ -10,48 +11,46 @@ from temporalio import activity
 
 @activity.defn(name="video-sprite")
 async def video_sprite(params) -> list[str]:
-    # TODO: this doesn't work
-    with TemporaryDirectory() as dir:
-        stream = ffmpeg.input(params["file"])
+    dir = tempfile.gettempdir()
+    stream = ffmpeg.input(params["file"])
 
-        interval = params.get("interval", 5)
-        expr = f"floor((t - prev_selected_t) / {interval})"
-        stream = stream.filter("select", expr=expr)
+    interval = params.get("interval", 5)
+    expr = f"floor((t - prev_selected_t) / {interval})"
+    stream = stream.filter("select", expr=expr)
 
-        if layout := params.get("layout"):
-            stream = stream.filter("tile", layout=f"{layout[0]}x{layout[1]}")
+    if layout := params.get("layout"):
+        stream = stream.filter("tile", layout=f"{layout[0]}x{layout[1]}")
 
-        stream = stream.filter(
-            "scale", width=params.get("width", -1), height=params.get("height", -1)
-        )
+    stream = stream.filter(
+        "scale", width=params.get("width", -1), height=params.get("height", -1)
+    )
 
-        filename = f"{dir}/%03d.png"
-        if count := params.get("count"):
-            stream = stream.output(filename, fps_mode="passthrough", vframes=count)
-        else:
-            stream = stream.output(filename, fps_mode="passthrough")
+    filename = f"{dir}/%03d.png"
+    if count := params.get("count"):
+        stream = stream.output(filename, fps_mode="passthrough", vframes=count)
+    else:
+        stream = stream.output(filename, fps_mode="passthrough")
 
-        stream.run()
+    stream.run()
 
-        paths = list(Path(dir).iterdir())
-        paths.sort(key=lambda p: int(p.stem))
-        return paths
+    paths = list(Path(dir).iterdir())
+    paths.sort(key=lambda p: int(p.stem))
+    return paths
 
 
 @activity.defn(name="video-transcode")
 async def video_transcode(params) -> str:
-    with TemporaryDirectory() as dir:
-        stream = ffmpeg.input(params["file"])
-
-        container = params.get("container", "mp4")
-        path = Path(f"{dir}/{uuid4()}.{container}")
-        kwargs = {
-            "codec:v": params.get("video-codec", "h264"),
-            "codec:a": params.get("audio-codec", "libopus"),
-        }
-        stream = stream.output(str(path), **kwargs)
-        stream.run()
-        return path
+    dir = tempfile.gettempdir()
+    stream = ffmpeg.input(params["file"])
+    container = params.get("container", "mp4")
+    path = f"{dir}/{uuid4()}.{container}"
+    kwargs = {
+        "codec:v": params.get("video-codec", "h264"),
+        "codec:a": params.get("audio-codec", "libopus"),
+    }
+    stream = stream.output(path, **kwargs)
+    stream.run()
+    return path
 
 
 @activity.defn(name="audio-waveform")
