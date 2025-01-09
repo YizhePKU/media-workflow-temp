763b80612dc1ea7c8086d21b642b215a81f28276
sunyizhe <sunyizhe@tezign.com>
2025-01-09 11:03:26 +0800

    feat: limit LibreOffice concurrency to prevent OOM

--- media_workflow/activities/document_to_pdf.py
+++ media_workflow/activities/document_to_pdf.py
@@ -1,12 +1,32 @@
 import asyncio
 import os
+from contextlib import asynccontextmanager
 from pathlib import Path
 from tempfile import mkdtemp
+from typing import AsyncGenerator
 
 from pydantic import BaseModel
 from temporalio import activity
 
 from media_workflow.trace import instrument
+
+# To run multiple instances of LibreOffice concurrently, they must have different profile directories.
+# We use a pool to limit the maximum concurrency, otherwise the worker can run out of memory.
+MAX_PROFILES = 4
+profiles = asyncio.Queue[Path](maxsize=MAX_PROFILES)
+
+# Initialize profiles.
+for _ in range(MAX_PROFILES):
+    profiles.put_nowait(Path(mkdtemp()))
+
+
+@asynccontextmanager
+async def get_profile() -> AsyncGenerator[Path]:
+    profile = await profiles.get()
+    try:
+        yield profile
+    finally:
+        await profiles.put(profile)
 
 
 class DocumentToPdfParams(BaseModel):
@@ -17,19 +37,21 @@ class DocumentToPdfParams(BaseModel):
 @activity.defn
 async def document_to_pdf(params: DocumentToPdfParams) -> Path:
     _dir = Path(mkdtemp(dir=os.environ["MEDIA_WORKFLOW_DATADIR"]))
-    process = await asyncio.subprocess.create_subprocess_exec(
-        "soffice",
-        # To run multiple instances of LibreOffice concurrently, they must have different profile directories.
-        f"-env:UserInstallation=file://{mkdtemp()}",
-        "--convert-to",
-        "pdf",
-        "--outdir",
-        _dir,
-        params.file,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    (stdout, stderr) = await process.communicate()
+    async with get_profile() as profile:
+        # heartbeat so that we abort the operation if waiting for a profile took too long
+        activity.heartbeat()
+        process = await asyncio.subprocess.create_subprocess_exec(
+            "soffice",
+            f"-env:UserInstallation=file://{profile}",
+            "--convert-to",
+            "pdf",
+            "--outdir",
+            _dir,
+            params.file,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        (stdout, stderr) = await process.communicate()
     if process.returncode != 0:
         raise RuntimeError(f"soffice failed: {stdout.decode()} {stderr.decode()}")
     output = _dir / f"{Path(params.file).stem}.pdf"
