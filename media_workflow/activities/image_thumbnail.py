50db579ee5b5488f1eafe02d73d7fee73fd5940d
sunyizhe <sunyizhe@tezign.com>
2025-01-20 14:05:34 +0800

    feat: use psd-tools to handle large psd files

--- media_workflow/activities/image_thumbnail.py
+++ media_workflow/activities/image_thumbnail.py
@@ -4,6 +4,7 @@ from tempfile import mkdtemp
 from uuid import uuid4
 
 import pyvips
+from psd_tools import PSDImage
 from pydantic import BaseModel
 from temporalio import activity
 
@@ -19,6 +20,17 @@ class ImageThumbnailParams(BaseModel):
 @activity.defn
 async def image_thumbnail(params: ImageThumbnailParams) -> Path:
     thumbnail = Path(mkdtemp(dir=os.environ["MEDIA_WORKFLOW_DATADIR"])) / f"{uuid4()}.jpeg"
-    pyvips.Image.thumbnail(params.file, params.size[0]).write_to_file(thumbnail)  # type: ignore
+
+    # pyvips supports PSD via ImageMagick, but it uses too much memory when processing PSD with a lot of layers. We'll
+    # use psd-tools instead, which can composite image layers much more efficiently (hopefully). On the other hand,
+    # psd-tools pulls in a lot of dependencies that I've been trying to avoid.
+    if params.file.suffix in [".psd", ".psb"]:
+        image = PSDImage.open(params.file).composite()
+        assert image is not None
+        image.thumbnail(params.size)
+        image.convert("RGB").save(thumbnail)
+    else:
+        pyvips.Image.thumbnail(params.file, params.size[0]).write_to_file(thumbnail)  # type: ignore
+
     assert thumbnail.exists()
     return thumbnail
